a4830354e4a1c54a2b97baef87cccf0058cacf39
Andriamahay11master <hirimanana@yahoo.fr>
2024-08-08 16:36:17 +0300

    init menu

---
+++ src/components/menu/Menu.tsx
@@ -0,0 +1,24 @@
+import { Link } from "react-router-dom"
+import { MenuType } from "../../models/MenuType"
+import './menu.scss'
+
+interface MenuProps {
+    linkMenu: MenuType[]
+}
+export default function Menu({linkMenu} : MenuProps) {
+    return (
+        <div className="menu">
+            <div className="menu-top">
+                <div className="menu-title">
+                    <h1>Snake 2D</h1>
+                    <img className="menu-img" src="/serpent.png"/>
+                </div>
+            </div>
+            <div className="menu-content">
+               <ul className="menu-list">
+                    {linkMenu.map((link, index) => <li key={index}><Link to={link.href}>{link.label}</Link></li>) }
+                </ul> 
+            </div>
+        </div>
+    )
+}
