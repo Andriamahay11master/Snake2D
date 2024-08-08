e5fc5a2e8f8e378d9fd1275bc6cc52733abe6d17
Andriamahay11master <hirimanana@yahoo.fr>
2024-08-08 17:26:42 +0300

    add CSS menu

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -8,16 +8,16 @@ interface MenuProps {
 export default function Menu({linkMenu} : MenuProps) {
     return (
         <div className="menu">
-            <div className="menu-top">
+            <div className="menu-body">
                 <div className="menu-title">
-                    <h1>Snake 2D</h1>
+                    <h1>Snake 2d</h1>
                     <img className="menu-img" src="/snake.gif"/>
                 </div>
-            </div>
-            <div className="menu-content">
-               <ul className="menu-list">
-                    {linkMenu.map((link, index) => <li key={index}><Link to={link.href}>{link.label}</Link></li>) }
-                </ul> 
+                <div className="menu-content">
+                    <ul className="menu-list">
+                        {linkMenu.map((link, index) => <li key={index}><Link to={link.href}>{link.label}</Link></li>) }
+                    </ul> 
+                </div>
             </div>
         </div>
     )
