00580095b061b8479ef51b2ac97ed556b4ceda45
Andriamahay11master <hirimanana@yahoo.fr>
2024-08-08 15:00:57 +0300

    init component game over

---
+++ src/components/over/Over.tsx
@@ -0,0 +1,12 @@
+export default function Over() {
+    return (
+        <div className="over-content">
+            <div className="over-top">
+                <p>Game Over</p>
+            </div>
+            <div className="over-bottom">
+                <button className="btn btn-icon"><i className="icon icon-retry"></i><span>Tap to retry</span></button>
+            </div>
+        </div>
+    )
+}
